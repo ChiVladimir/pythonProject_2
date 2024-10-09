d00674d4adf4c28bfc6f66340278f06604f3d3fa
Владимир <chivladimir@yandex.ru>
2024-10-09 14:11:01 +0300

    Модуль 5 Лекция 5, дз 3 (доработанное)

--- .venv/Module_5_hw_3.py
+++ .venv/Module_5_hw_3.py
@@ -1,4 +1,4 @@
-# Классы и объекты
+# Классы и объекты. Перегрузка операторов.
 
 
 class House:
@@ -10,33 +10,48 @@ class House:
         return self.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
-
-    def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
-
-    def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
-
-    def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
-
-    def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt__(other)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def __add__(self, value):
-        self.number_of_floors = self.number_of_floors + value
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
     def __radd__(self, value):
-        self.number_of_floors = self.number_of_floors + value
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
     def __iadd__(self, value):
-        self.number_of_floors = self.number_of_floors + value
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
     def __str__(self):
---
+++ .venv/Module_5_lesson_5.py
@@ -0,0 +1,44 @@
+# Различие атрибутов класса и экземпляра. Пространство имен класса.
+
+
+class Human:
+
+    head = True
+
+    def __init__(self, name, age): # dunder - double ander
+        self.name = name
+        self.age = age
+        self.say_info()
+
+    def say_info(self):
+        print (f'Hello, my name is {self.name}, I have {self.age} years old')
+
+    def birthday(self):
+        self.age += 1
+        print(f'I have Birthday today, I have {self.age} now')
+
+    def __len__(self):
+        return self.age
+
+    def __lt__(self, other):
+        return self.age < other.age
+
+    def __gt__(self, other):
+        return self.age > other.age
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __del__(self):
+        print(f'{self.name} is over')
+
+
+den = Human('Den', 22)
+max = Human('Max', 22)
+
+a = 6
+
+print(Human.head)
