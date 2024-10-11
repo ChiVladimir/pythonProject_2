1ea275ddd789e4b4cdbbe96447a65c782215cf1f
Владимир <chivladimir@yandex.ru>
2024-10-11 18:26:11 +0300

    Модуль 5 Дополнительная практика (доработка проверки корректности пароля)

--- .venv/Module_5_additional_practice.py
+++ .venv/Module_5_additional_practice.py
@@ -22,48 +22,52 @@ class User:
 def check_pswd(pswd):
     overlap = set()
     check = True
+    reason = str
     letters_app ={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
     letters_low = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
     symbols = {'!', '#', '$', '%', '^', '&', '*', '(', ')', '—', '_ ', '+', '=', ';', ':', ',' '.', '/', '?', '|', '`', '~', '[', ']'}
     numbers = {'0','1','2','3','4','5','6','7','8','9'}
-    print(letters_app)
-    print(letters_low)
-    print(symbols)
-    print(numbers)
 
     i = 0
     for i in range(len(pswd)):
         overlap.add(pswd[i])
         i+=1
-    print (overlap)
     if len(overlap) < 6:
         check = False
-        return check, "The password must contain more than 6 characters"
+        reason = "The password must contain more than 6 characters"
+        print(reason)
     elif len(list(overlap & letters_app)) == 0:
         check = False
-        return check, "The password must contain at least one uppercase letter"
+        reason =  "The password must contain at least one uppercase letter"
     elif len(list(overlap & letters_low)) == 0:
         check = False
-        return check, "The password must contain at least one lowercase letter "
+        reason =  "The password must contain at least one lowercase letter "
     elif len(list(overlap & symbols)) == 0:
         check = False
-        return check, "The password must contain at least one special symbol"
+        reason =  "The password must contain at least one special symbol"
     elif len(list(overlap & numbers)) == 0:
         check = False
-        return check, "The password must contain at least one digit"
-
+        reason =  "The password must contain at least one digit"
+    return check, reason
 
 if __name__ == '__main__':
 
     database = Database()
     while True:
 #        choice = input('Hello! Select the action: \n1 - Enter\n2 - Registration:\n')
-        user = User(input("Enter username   : "), password1 := input("Enter password   : "), password2 := input("Re-Enter password: ")) # "моржовый оператор"
+        user = User(username :=input("Enter username   : "), password1 := input("Enter password   : "), password2 := input("Re-Enter password: ")) # "моржовый оператор"
+        print (username)
+        print (password1)
+        print (password2)
         if password1 != password2:
-            print("Password wrong!")
+            print("Passwords don't match!")
             exit()
-        p1 = check_pswd(password1)
+        flag, reason = check_pswd(password1)
+        if flag == False:
+            print (reason)
+            exit()
     database.add_user(user.username, user.password)
-
-
+    print("Successful complete!")
+    exit()
 print(database.data)
+exit()
