0fbcdc1edeed0051eaf417632f0c97d8d0c39e6e
Владимир <chivladimir@yandex.ru>
2024-10-24 14:06:57 +0300

    Модуль 6, финальная работа

---
+++ .venv/Module_6_hard.py
@@ -0,0 +1,67 @@
+#Модуль 6. Дополнительная практика
+#Задание "Они все так похожи"
+
+
+class Figure:
+    sides_count = 0
+
+    def __init__(self):
+        self.__sides = tuple
+        self.__color = tuple
+        self.filled =bool
+
+    def get_color(self):
+        return self._color
+
+    def __is_valid_color(self, r, g, b):
+        if 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255:
+            self.r = r, self.g = g, self.b = b
+
+    def set_color(self, r, g, b):
+        __is_valid_color(r, g, b)
+
+    def is_valid_sides(self, *args):
+        self.valid_sides = False
+        neg = tuple(i for i in args if i < 0)
+        if len(args) == self.__sides:
+            self.valid_sides = True
+            if len(neg) > 0:
+                self.valid_sides = False
+        return self.valid_sides
+
+    def get_sides(self):
+        return __sides
+
+    def __len__(self, __sides):
+        return sum(__sides)
+
+    def set_sides(self, *new_sides):
+        if is_valid_sides():
+            __sides = new_sides
+
+
+
+
+
+
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15) # Изменится
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
+
