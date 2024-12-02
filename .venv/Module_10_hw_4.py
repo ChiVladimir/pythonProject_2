774c3c45d0472673b84fc33c25b52fc1ff6b943b
Владимир <chivladimir@yandex.ru>
2024-12-02 11:37:27 +0300

    Module 10, lesson 4, homework 4

--- .venv/Module_10_hw_4.py
+++ .venv/Module_10_hw_4.py
@@ -15,6 +15,7 @@ class Table:
 class Guest(Thread):
     def __init__(self, name):
         threading.Thread.__init__(self)
+        super().__init__()
         self.name = name
 
     def run(self):
@@ -26,43 +27,42 @@ class Cafe:
         self.queue = Queue()
         self.tables = list(tables)
 
+
     def guest_arrival(self, *args):
-        list_guests = list(args)
-        list_tables = self.tables
+        for guest in args:
+            if self.non_busy_table() is not None:
+                thread_1 = guest
+                thread_1.start()
+                table = self.non_busy_table()
+                self.guests_vs_tables[table] = thread_1
+                table.guest = thread_1
+                print(f'{guest.name} сел(-а) за стол номер {table.number}')
+            else:
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
 
-        for i in range(len(list_tables)):
-            thread_1 = list_guests[i]
-            thread_1.start()
-            Cafe.guests_vs_tables[list_tables[i]] = list_guests[i]
-            print(f'{list_guests[i].name} сел(-а) за стол номер {list_tables[i].number}')
-        if len(list_guests) > (len(list_tables) - len(list_tables)):
-            for i in range((len(list_tables) - len(list_tables)), len(list_guests)):
-                self.queue.put(guests[i])
-                print(f'{list_guests[i].name} в очереди')
-
-    def busy_table(self):
+    def non_busy_table(self):
         for table in self.tables:
-            print(table.guest)
-            if table.guest is not None:
-                return True
+            if table.guest is None:
+                return table
+        return None
 
     def discuss_guests(self):
-        print(self.queue.empty(), Cafe.busy_table(self))
-        print(self.guests_vs_tables)
-        while not (self.queue.empty()) or Cafe.busy_table(self):
+
+        while not (self.queue.empty()) or Cafe.non_busy_table(self):
             for table in self.tables:
-                print (self.tables)
-                if table.guest is not None and not (table.guest.is_alive()):
+
+                if table.guest is not None and not table.guest.is_alive():
                     print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
                     print(f'Стол номер {table.number} свободен')
                     table.guest = None
-                if self.queue.empty() is not True and table.guest is None:
-                    print(self.queue.empty(), table.guest)
-                    table.guest = self.queue.get()
-                    print(f'{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
-                    thread_2 = table.guest
-                    thread_2.start()
-                    Cafe.guests_vs_tables[table.number] = table.guest.name
+                    if self.queue.empty() is not True and table.guest is None:
+                        table.guest = self.queue.get()
+                        print(f'{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                        thread_2 = table.guest
+                        thread_2.start()
+                        Cafe.guests_vs_tables[table] = thread_2
+
 
 
 # Создание столов
